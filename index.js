795e058f7ff70cd06827fe60bc661e4fdf13ca99
unknown <mhriad786@gmail.com>
2024-11-27 03:39:14 +0600

    add booking delete route

--- index.js
+++ index.js
@@ -65,6 +65,15 @@ async function run() {
         const result = await bookingCollection.find(query).toArray()
         res.send(result)  
     })
+    app.delete("/booking/:id",async (req , res)=>{
+        
+      const id = req.params.id
+
+      const query = {_id: new ObjectId(id)}
+        
+        const result = await bookingCollection.deleteOne(query)
+        res.send(result)  
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
